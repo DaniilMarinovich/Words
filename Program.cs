881b52e12a3cc364dc79f00d9d396a74c6cb3407
DaniilMarinovich <marinovich.daniil@gmail.com>
2024-11-08 19:03:41 +0300

    Add players

--- Program.cs
+++ Program.cs
@@ -5,12 +5,28 @@ namespace Words;
 
 public class Words
 {
-
+    private static Dictionary<string, int> wordLetters;
     private static string word;
+
+    private static string Player1;
+    private static string Player2;
+    private static List<string> Player1Words = new List<string>();
+    private static List<string> Player2Words = new List<string>();
 
     public static void Main(string[] args)
     {
+        ReadWord();
+        PlayersNames();
 
+        for (int i = 0; i < 10; i++)
+        {
+            AddWord(Player1, "ckjad");
+            AddWord(Player2, "ckjad");
+        }
+
+        DisplayWords();
+
+        Console.WriteLine(word);
     }
 
     private static string ReadWord()
@@ -57,5 +73,44 @@ public class Words
         Console.WriteLine(message);
 
         Console.ResetColor();
+    }
+
+    private static void PlayersNames()
+    {
+        Console.WriteLine("Введите имя первого игрока:");
+        Player1 = Console.ReadLine();
+
+        Console.WriteLine("Введите имя второго игрока:");
+        Player2 = Console.ReadLine();
+    }
+
+    private static void AddWord(string player, string word)
+    {
+        if (player == Player1)
+        {
+            Player1Words.Add(word);
+        }
+        else if (player == Player2)
+        {
+            Player2Words.Add(word);
+        }
+        else
+        {
+            ExceptionConsoleMessage("Неверный игрок");
+        }
+    }
+
+    private static void DisplayWords()
+    {
+        Console.BackgroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine($"|{"Слова первого игрока".PadRight(30)}|{"Слова второго игрока".PadRight(30)}|");
+        Console.WriteLine(new string('-', 63));
+        for (int i = 0; i < Math.Max(Player1Words.Count, Player2Words.Count); i++)
+        {
+
+            Console.WriteLine($"|{(i < Player1Words.Count ? Player1Words[i] : String.Empty ).PadRight(30)}|{(i < Player2Words.Count ? Player1Words[i] : String.Empty).PadRight(30)}|");
+        }
+        Console.WriteLine(new string('-', 63));
+        Console.WriteLine();
     }
 }
