fac492d8a204051d1db442f1584fb7c9ceaa9583
DaniilMarinovich <marinovich.daniil@gmail.com>
2024-11-15 18:35:05 +0300

    Add timer mod

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Timers;
 
 namespace Words;
 
@@ -16,11 +17,83 @@ public class Words
     private const int MinLength = 8;
     private const int MaxLength = 30;
 
+    private static Timer timer;
+    private static bool isTimerModeOn = false;
+    private static int remainingTime;
+    private static bool isTimeUp = false;
+    private static int turnTime;
+
     public static void Main(string[] args)
     {
+        Menu();
         CreateDictionary(ReadWord());
         InputPlayersNames();
         GameProcess(player1);
+    }
+
+    private static void Menu() 
+    {
+        while (true)
+        {
+            Console.WriteLine("\nМеню:" +
+                "\n\t1. Выбор языка игры." +
+                "\n\t2. Выбор режима игры." +
+                "\n\t3. Начать игру.");
+
+            switch(Console.ReadLine())
+            {
+                case "1":
+                    break;
+                case "2":
+                    SelectTimer();
+                    break;
+                case "3":
+                    return;
+                default:
+                    ExceptionConsoleMessage("Неверный выбор. Повторите выбор.");
+                    break;
+            }
+        }
+    }
+
+    private static void SelectTimer()
+    {
+        Console.WriteLine("Добро пожаловать в меню выбора таймера:" +
+            "\n\t1. 2 минуты." +
+            "\n\t2. 1 минута." +
+            "\n\t3. 30 секунд." +
+            "\n\t4. 10 секунд." +
+            "\n\t5. Продолжить без таймера.");
+
+        while (!isTimerModeOn)
+        {
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    SetTimer(120);
+                    return;
+                case "2":
+                    SetTimer(60);
+                    return;
+                case "3":
+                    SetTimer(30);
+                    return;
+                case "4":
+                    SetTimer(10);
+                    return;
+                case "5":
+                    return;
+                default:
+                    ExceptionConsoleMessage("Неверный выбор. Повторите выбор.");
+                    break;
+            }
+        }
+    }
+
+    private static void SetTimer(int time)
+    {
+        isTimerModeOn = true;
+        turnTime = time;
     }
 
     private static string ReadWord()
@@ -143,12 +216,13 @@ public class Words
     private static void WordsOutput()
     {
         MarkupConsoleMessage($"|{$"Слова игрока {player1}".PadRight(MaxLength)}|{$"Слова игрока {player2}".PadRight(MaxLength)}|");
-        MarkupConsoleMessage(new string('-', MaxLength*3 + 3));
+        MarkupConsoleMessage(new string('-', MaxLength*2 + 3));
         for (int i = 0; i < Math.Max(player1Words.Count, player2Words.Count); i++)
         {
             MarkupConsoleMessage($"|{(i < player1Words.Count ? player1Words[i] : String.Empty ).PadRight(MaxLength)}|{(i < player2Words.Count ? player2Words[i] : String.Empty).PadRight(MaxLength)}|");
         }
-        MarkupConsoleMessage(new string('-', MaxLength*3 + 3));
+        MarkupConsoleMessage($"|{$"Количество слов : {player1Words.Count}".PadRight(MaxLength)}|{$"Количество слов : {player2Words.Count}".PadRight(MaxLength)}|");
+        MarkupConsoleMessage(new string('-', MaxLength*2 + 3));
         Console.WriteLine();
     }
 
@@ -161,29 +235,71 @@ public class Words
         Console.ResetColor();
     }
 
+    private static void StartTimer()
+    {
+        remainingTime = turnTime;
+        isTimeUp = false;
+        timer = new Timer(1000);
+        timer.Elapsed += TimerElapsed;
+        timer.Start();
+    }
+
+        private static void TimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            remainingTime--;
+
+            if (remainingTime <= 0)
+            {
+                isTimeUp = true;
+                timer.Stop();
+                Console.WriteLine("\nВремя вышло! Ход игрока завершен.");
+            }
+        }
+
     private static void GameProcess(string player)
     {
         Console.WriteLine($"\nХод игрока {player}");
 
         string playerWord;
         int playerAttempts = 3;
-
-        do
+        if(isTimerModeOn)
         {
-            playerWord = Console.ReadLine().ToLower();
-            if (CheckWordFromOriginalLetters(playerWord) && CheckWordUniqueness(playerWord))
-            {
-                AddWord(player, playerWord);
-                MarkupConsoleMessage("Слово подходит!\n");
-                GameProcess(player == player1 ? player2 : player1);
-            }
-            else
-            {
-                ExceptionConsoleMessage($"Количество оставшихся попыток: {--playerAttempts}");
-            }
-        } while (playerAttempts > 0);
+            StartTimer();
+        }
 
-        GameProcessFinish(player == player1 ? player2 : player1);
+        while (playerAttempts > 0 && !isTimeUp)
+        {
+            if (Console.KeyAvailable)
+            {
+                playerWord = Console.ReadLine().ToLower();
+                if (CheckWordFromOriginalLetters(playerWord) && CheckWordUniqueness(playerWord))
+                {
+                    AddWord(player, playerWord);
+                    MarkupConsoleMessage("Слово подходит!\n");
+                    if (isTimerModeOn)
+                    {
+                        timer.Stop();
+                    }
+                    GameProcess(player == player1 ? player2 : player1);
+                }
+                else
+                {
+                    ExceptionConsoleMessage($"Количество оставшихся попыток: {--playerAttempts}");
+                }
+            }
+        }
+
+        if (isTimerModeOn)
+        {
+            if(isTimeUp)
+            {
+                GameProcessFinish(player == player1 ? player2 : player1);
+            }
+        }
+        else
+        {
+            GameProcessFinish(player == player1 ? player2 : player1);
+        }
     }
 
     private static void GameProcessFinish(string player)
