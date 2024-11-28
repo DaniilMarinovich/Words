be5475c98df87161dff905182cfa1311dc43765a
DaniilMarinovich <marinovich.daniil@gmail.com>
2024-11-28 17:44:25 +0300

    Add player model

---
+++ Models/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Words.Models;
+
+public  class Player
+{
+    public string Name {get; private set; }
+    public List<string> Words { get; private set; }
+
+    Player(string name) 
+    {
+        Name = name;
+        Words = new List<string>();
+    }
+
+    public void AddWord(string word)
+    {
+        Words.Add(word);
+    }
+}
